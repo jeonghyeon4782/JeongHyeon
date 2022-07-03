446a1bb0f8448166bd04405d00d9dda171de6978
jeonghyeon4782 <wjd4782@naver.com>
2022-07-03 19:52:08 +0900

    .

--- hello/src/helloworld/Main.java
+++ hello/src/helloworld/Main.java
@@ -3,6 +3,6 @@ package helloworld;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("안녕");
-		System.out.println("수정사항123");
+		System.out.println("수정사항1234");
 	}
 }
