92aeaaae357b28e70ccc110c260e06cc8ee4be80
jeonghyeon4782 <wjd4782@naver.com>
2022-07-03 20:55:33 +0900

    .

--- hello/src/helloworld/Main.java
+++ hello/src/helloworld/Main.java
@@ -1,7 +1,9 @@
 package helloworld;
 
+import java.util.*;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("이클립스 어렵다 너무!");
+		System.out.println("한글 패치");
 	}
 }
