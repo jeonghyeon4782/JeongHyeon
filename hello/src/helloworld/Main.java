565283e696f70be393d3f3eaf5a718e661dcf0f6
jeonghyeon4782 <wjd4782@naver.com>
2022-07-04 00:08:50 +0900

    예외처리

--- hello/src/helloworld/Main.java
+++ hello/src/helloworld/Main.java
@@ -1,9 +1,28 @@
 package helloworld;
 
-import java.util.*;
+class MyException extends Exception {
+	int x;
+
+	public MyException(int x) {
+		this.x = x;
+	}
+	
+	public String toString() {
+		return "I am " + x;
+	}
+	
+}
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("한글 패치");
+		MyException me = new MyException(10);
+		
+		try {
+			System.out.println("예외를 던지기 전");
+			throw me;
+		} catch (MyException e) {
+			System.out.println(e);
+		}
+		System.out.println("end");
 	}
 }
