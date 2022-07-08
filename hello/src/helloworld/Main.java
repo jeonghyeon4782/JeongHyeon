85d03dc95f6cf6cc2beebdabfdd44e520ce68ad7
jeonghyeon4782 <wjd4782@naver.com>
2022-07-08 19:26:24 +0900

    .

--- hello/src/helloworld/Main.java
+++ hello/src/helloworld/Main.java
@@ -1,29 +1,15 @@
 package helloworld;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		bw.write(Arrays.toString(arr));
-		bw.flush();
-		bw.close();
-		br.close();
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 6;
+		
+		System.out.println(a);
+		System.out.println(b);
 	}
 }
