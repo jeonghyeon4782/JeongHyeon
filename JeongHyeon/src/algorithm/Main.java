2243f98876775b70fc4bfa50ee0e8a7f6abf6c14
jeonghyeon4782 <wjd4782@naver.com>
2022-07-13 22:25:15 +0900

    .

--- JeongHyeon/src/algorithm/Main.java
+++ JeongHyeon/src/algorithm/Main.java
@@ -1,7 +1,24 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int max = a;
+		
+		if (max < b) {
+			max = b;
+		}
+		else if (max < c) {
+			max = c;
+		}
+		
+		System.out.println("최댓값은 " + max + "입니다.");
 	}
 }
